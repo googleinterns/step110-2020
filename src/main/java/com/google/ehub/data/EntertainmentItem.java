24141335f2c80b4a6aff6d8af7bdb9300b51b5ba
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-06-23 19:59:30 +0000

    Make EntertainmentItem member variables final

--- src/main/java/com/google/ehub/data/EntertainmentItem.java
+++ src/main/java/com/google/ehub/data/EntertainmentItem.java
@@ -3,10 +3,10 @@ package com.google.ehub.data;
 /**
  * Holds information for Entertainment Items used in Datastore.
  */
-public class EntertainmentItem {
-  private String title;
-  private String description;
-  private String imageURL;
+public final class EntertainmentItem {
+  private final String title;
+  private final String description;
+  private final String imageURL;
 
   public EntertainmentItem(String title, String description, String imageURL) {
     this.title = title;
