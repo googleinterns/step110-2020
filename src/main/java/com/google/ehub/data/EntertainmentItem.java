332cdf71f35c31666e7556f2a84f96a4ab5d1b43
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-06-25 19:31:03 +0000

    Fix naming nit change ID into Id

--- src/main/java/com/google/ehub/data/EntertainmentItem.java
+++ src/main/java/com/google/ehub/data/EntertainmentItem.java
@@ -4,20 +4,20 @@ package com.google.ehub.data;
  * Holds information for Entertainment Items used in Datastore.
  */
 public final class EntertainmentItem {
-  private final long uniqueID;
+  private final long uniqueId;
   private final String title;
   private final String description;
   private final String imageURL;
 
-  public EntertainmentItem(long uniqueID, String title, String description, String imageURL) {
-    this.uniqueID = uniqueID;
+  public EntertainmentItem(long uniqueId, String title, String description, String imageURL) {
+    this.uniqueId = uniqueId;
     this.title = title;
     this.description = description;
     this.imageURL = imageURL;
   }
 
-  public long getUniqueID() {
-    return uniqueID;
+  public long getUniqueId() {
+    return uniqueId;
   }
 
   public String getTitle() {
