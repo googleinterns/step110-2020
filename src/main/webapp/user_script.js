43ef2a5a20e1f3c827d7a36ec3ba8ce9b48934b5
Oyin Sholeye <oyin.s@wustl.edu>
2020-06-29 18:23:39 +0000

    Users are able to login, create and see Profile

--- src/main/webapp/user_script.js
+++ src/main/webapp/user_script.js
@@ -1,9 +1,12 @@
-
 function loadProfile() {
-  fetch('/profile-data')
-    .then((response) => response.json())
-    .then((profile) => {
-      var profileContainer = document.getElementById('profile-container');
-      commentElement.innerText = profile;
-    });
+  fetch('/profile-data').then((response) => response.json()).then((profile) => {
+    var nameSection = document.getElementById('name');
+    nameSection.innerHTML = profile.name;
+
+    var usernameSection = document.getElementById('username');
+    usernameSection.innerHTML = profile.username;
+
+    var bioSection = document.getElementById('bio');
+    bioSection.innerHTML = profile.bio;
+  });
 }
