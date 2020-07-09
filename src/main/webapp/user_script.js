ba619c281d82fedc3567d4c287f95bc3e4fd6ae6
Oyin Sholeye <oyin.s@wustl.edu>
2020-07-09 16:24:38 +0000

    Added prepopulation functionality in user_script.js

--- src/main/webapp/user_script.js
+++ src/main/webapp/user_script.js
@@ -8,12 +8,32 @@ function loadProfile() {
     .then((profile) => {
       const nameSection = document.getElementById('name');
       nameSection.innerHTML = profile.name;
+      nameSection.value=profile.name;
 
       const usernameSection = document.getElementById('username');
+      usernameSection.innerHTML = profile.username;
       usernameSection.innerHTML = profile.username;
 
       const bioSection = document.getElementById('bio');
       bioSection.innerHTML = profile.bio;
+    })
+    .catch((error) => {
+      console.log('Fetching profile data servlet failed: ' + error);
+    });
+}
+
+function loadPopulatedProfile() {
+  fetch('/profile-data')
+    .then((response) => response.json())
+    .then((profile) => {
+      const nameSection = document.getElementById('name');
+      nameSection.value=profile.name;
+
+      const usernameSection = document.getElementById('username');
+      usernameSection.value = profile.username;
+      
+      const bioSection = document.getElementById('bio');
+      bioSection.value = profile.bio;
     })
     .catch((error) => {
       console.log('Fetching profile data servlet failed: ' + error);
