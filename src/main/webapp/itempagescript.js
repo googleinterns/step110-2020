275066920541fe7d870714b4cd081d1b252128aa
Eric Irikannu <eeirikannu@gmail.com>
2020-07-10 16:16:31 +0000

    Up to date code

--- src/main/webapp/itempagescript.js
+++ src/main/webapp/itempagescript.js
@@ -5,11 +5,11 @@ async function loadItemPage() {
   const itemId = getItemId();
   if (itemId !== '') {
     fetch(`/itempagedata?itemId=${itemId}`)
-        .then((response) => response.json())
-        .then((itemPageData) => {
-          createSelectedItemCard(itemPageData.item);
-          getItemPageComments(itemPageData.comments);
-        });
+      .then((response) => response.json())
+      .then((itemPageData) => {
+        createSelectedItemCard(itemPageData.item);
+        getItemPageComments(itemPageData.comments);
+      });
   } else {
     console.log('ItemId is empty!');
   }
@@ -21,17 +21,17 @@ async function loadItemPage() {
 async function createSelectedItemCard(entertainmentItem) {
   const card = $('<div class="card bg-light"></div>');
   card.append(
-      $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">'));
+    $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">'));
   const cardBody = $('<div class="card-body"></div>');
   cardBody.append(
-      $('<h5 class="card-title">' + entertainmentItem.title + '(' +
-        entertainmentItem.releaseDate + ')' +
-        '</h5>'));
+    $('<h5 class="card-title">' + entertainmentItem.title + '(' +
+      entertainmentItem.releaseDate + ')' +
+      '</h5>'));
   cardBody.append(
-      $('<h5 class="card-title">' + entertainmentItem.genre + '</h5>'));
+    $('<h5 class="card-title">' + entertainmentItem.genre + '</h5>'));
   cardBody.append(
-      $('<p class="card-text"><b>Description: </b>' +
-        entertainmentItem.description + '</p>'));
+    $('<p class="card-text"><b>Description: </b>' +
+      entertainmentItem.description + '</p>'));
 
   card.append(cardBody);
   const itemContainer = $('#item-container');
@@ -44,13 +44,13 @@ async function createSelectedItemCard(entertainmentItem) {
 async function sendFormData() {
   const comment = $('#comment').val();
   const itemId = getItemId();
-  $.post('/itempagedata', {comment: comment, itemId: itemId})
-      .done(function() {
-        window.location.reload();
-      })
-      .fail(function() {
-        console.log('Failed to send form data');
-      });
+  $.post('/itempagedata', { comment: comment, itemId: itemId })
+    .done(function() {
+      window.location.reload();
+    })
+    .fail(function() {
+      console.log('Failed to send form data');
+    });
 }
 
 /**
@@ -61,18 +61,18 @@ function getItemPageComments(comments) {
   comments.forEach((commentDataManager) => {
     const date = new Date(commentDataManager.timestampMillis);
     commentContainer.append(createListElement(
-        commentDataManager.comment + ' - ' +
-        '(' + date.toLocaleString() + ')'));
+      commentDataManager.comment + ' - ' +
+      '(' + date.toLocaleString() + ')'));
   });
 }
 
 /**	
- * Gets itemId from current URL	 
- */	 
-function getItemId() {	
-  const queryString = window.location.search;	 
-  const urlParams = new URLSearchParams(queryString);	 
-  return urlParams.get('itemId');	 
+ * Gets itemId from current URL.
+ */
+function getItemId() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get('itemId');
 }
 
 /**
