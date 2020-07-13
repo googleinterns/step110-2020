377c6bb61399347ae0f28c7c79363bf723a83ad8
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-07-13 15:02:27 +0000

    Implement query to sort items by recent release date and include it as an option for DashboardServlet

--- src/main/java/com/google/ehub/data/EntertainmentItemDatastore.java
+++ src/main/java/com/google/ehub/data/EntertainmentItemDatastore.java
@@ -11,7 +11,7 @@ import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.QueryResultList;
-import com.google.ehub.utility.DatastoreUtils;
+import com.google.ehub.utility.Utils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +26,8 @@ public final class EntertainmentItemDatastore {
   private static final String DESCRIPTION_PROPERTY_KEY = "description";
   private static final String IMAGE_URL_PROPERTY_KEY = "imageUrl";
   private static final String RELEASE_DATE_PROPERTY_KEY = "releaseDate";
+  private static final String RELEASE_DATE_TIMESTAMP_MILLIS_PROPERTY_KEY =
+      "releaseDateTimestampMillis";
   private static final String RUNTIME_PROPERTY_KEY = "runtime";
   private static final String GENRE_PROPERTY_KEY = "genre";
   private static final String DIRECTORS_PROPERTY_KEY = "directors";
@@ -33,15 +35,18 @@ public final class EntertainmentItemDatastore {
   private static final String ACTORS_PROPERTY_KEY = "actors";
   private static final String OMDB_ID_PROPERTY_KEY = "omdbId";
 
+  private static final String RELEASE_DATE_FORMAT = "dd MMM yyyy";
+
   private static EntertainmentItemDatastore instance;
 
-  private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+  private final DatastoreService datastoreService =
+      DatastoreServiceFactory.getDatastoreService();
 
   private EntertainmentItemDatastore() {}
 
   /**
-   * Gives access to the single instance of the class, and creates this instance if it was not
-   * initialized previously.
+   * Gives access to the single instance of the class, and creates this instance
+   * if it was not initialized previously.
    *
    * @return single instance of the class
    */
@@ -63,10 +68,14 @@ public final class EntertainmentItemDatastore {
 
     // Unique Id is created by Datastore so it's not added as a property.
     itemEntity.setProperty(DISPLAY_TITLE_PROPERTY_KEY, item.getTitle());
-    itemEntity.setProperty(NORMALIZED_TITLE_PROPERTY_KEY, item.getTitle().toLowerCase());
+    itemEntity.setProperty(NORMALIZED_TITLE_PROPERTY_KEY,
+                           item.getTitle().toLowerCase());
     itemEntity.setProperty(DESCRIPTION_PROPERTY_KEY, item.getDescription());
     itemEntity.setProperty(IMAGE_URL_PROPERTY_KEY, item.getImageUrl());
     itemEntity.setProperty(RELEASE_DATE_PROPERTY_KEY, item.getReleaseDate());
+    itemEntity.setProperty(RELEASE_DATE_TIMESTAMP_MILLIS_PROPERTY_KEY,
+                           Utils.getTimestampMillisFromDate(
+                               item.getReleaseDate(), RELEASE_DATE_FORMAT));
     itemEntity.setProperty(RUNTIME_PROPERTY_KEY, item.getRuntime());
     itemEntity.setProperty(GENRE_PROPERTY_KEY, item.getGenre());
     itemEntity.setProperty(DIRECTORS_PROPERTY_KEY, item.getDirectors());
@@ -80,20 +89,24 @@ public final class EntertainmentItemDatastore {
   /**
    * Finds a single entertainment item based on unique id.
    *
-   * @param uniqueId id used to identify the EntertainmentItem Entity in the Datastore
-   * @return the EntertainmentItem found in Datastore wrapped in an {@link Optional}, the
+   * @param uniqueId id used to identify the EntertainmentItem Entity in the
+   *     Datastore
+   * @return the EntertainmentItem found in Datastore wrapped in an {@link
+   *     Optional}, the
    * optional object will be empty if the EntertainmentItem Entity was not found
    */
   public Optional<EntertainmentItem> queryItem(long uniqueId) {
     return queryItemByProperty(
-        Entity.KEY_RESERVED_PROPERTY, KeyFactory.createKey(ENTERTAINMENT_ITEM_KIND, uniqueId));
+        Entity.KEY_RESERVED_PROPERTY,
+        KeyFactory.createKey(ENTERTAINMENT_ITEM_KIND, uniqueId));
   }
 
   /**
    * Finds a single entertainment item based on ombd Id.
    *
    * @param omdbId the omdbId that the item is supposed to have
-   * @return the EntertainmentItem found in Datastore wrapped in an {@link Optional}, the
+   * @return the EntertainmentItem found in Datastore wrapped in an {@link
+   *     Optional}, the
    * optional object will be empty if the EntertainmentItem Entity was not found
    */
   public Optional<EntertainmentItem> queryItemByOmdbId(String omdbId) {
@@ -104,51 +117,62 @@ public final class EntertainmentItemDatastore {
    * Queries all entertainment items found in Datastore.
    *
    * @param fetchOptions the fetch options used by the resulting query
-   * @return list with all entertainment items found in Datastore, the list will be empty if no
-   *     items were found
+   * @return list with all entertainment items found in Datastore, the list will
+   *     be empty if no items were found
    */
   public EntertainmentItemList queryAllItems(FetchOptions fetchOptions) {
-    return createItemListFromQuery(fetchOptions, new Query(ENTERTAINMENT_ITEM_KIND));
-  }
-
-  /**
-   * Queries all entertainment items found in Datastore and orders them based on title with the
-   * given sorting direction.
-   *
-   * @param fetchOptions the fetch options used by the resulting query
-   * @param sortDirection the sort direction used to order the entertainment items based on title
-   * @return list with all the entertainment items following the specified sort direction, the list
-   *     will be empty if no items were found
-   */
-  public EntertainmentItemList queryAllItemsWithTitleOrder(
-      FetchOptions fetchOptions, SortDirection sortDirection) {
     return createItemListFromQuery(fetchOptions,
-        new Query(ENTERTAINMENT_ITEM_KIND).addSort(NORMALIZED_TITLE_PROPERTY_KEY, sortDirection));
+                                   new Query(ENTERTAINMENT_ITEM_KIND));
   }
 
   /**
-   * Queries entertainment items with the specified title prefix and sorting direction.
+   * Queries entertainment items with the specified title prefix and sorting
+   * direction.
    *
    * @param fetchOptions the fetch options used by the resulting query
    * @param titlePrefix the title prefix used to filter the query
-   * @param sortDirection the sort direction used to order the entertainment items based on title
-   * @return list with the entertainment items that match the title prefix and sorting direction,
-   *     the list will be empty if no items were found
+   * @param sortDirection the sort direction used to order the entertainment
+   *     items based on title
+   * @return list with the entertainment items that match the title prefix and
+   *     sorting direction, the list will be empty if no items were found
    */
-  public EntertainmentItemList queryItemsByTitlePrefix(
-      FetchOptions fetchOptions, String titlePrefix, SortDirection sortDirection) {
-    return createItemListFromQuery(fetchOptions,
-        new Query(ENTERTAINMENT_ITEM_KIND)
-            .addSort(NORMALIZED_TITLE_PROPERTY_KEY, sortDirection)
-            .setFilter(DatastoreUtils.getPrefixFilter(
-                NORMALIZED_TITLE_PROPERTY_KEY, titlePrefix.toLowerCase())));
+  public EntertainmentItemList
+  queryItemsByTitlePrefix(FetchOptions fetchOptions, String titlePrefix,
+                          SortDirection sortDirection) {
+    Query query = new Query(ENTERTAINMENT_ITEM_KIND)
+                      .addSort(NORMALIZED_TITLE_PROPERTY_KEY, sortDirection);
+
+    if (!titlePrefix.isEmpty()) {
+      query = query.setFilter(Utils.getPrefixFilter(
+          NORMALIZED_TITLE_PROPERTY_KEY, titlePrefix.toLowerCase()));
+    }
+
+    return createItemListFromQuery(fetchOptions, query);
   }
 
-  private Optional<EntertainmentItem> queryItemByProperty(
-      String propertyName, Object propertyValue) {
-    Query query =
-        new Query(ENTERTAINMENT_ITEM_KIND)
-            .setFilter(new FilterPredicate(propertyName, FilterOperator.EQUAL, propertyValue));
+  /**
+   * Queries entertainment items based on recent release date.
+   *
+   * @param fetchOptions the fetch options used by the resulting query
+   * @param sortDirection the sort direction used to order the entertainment
+   *     items based on release date
+   * @return list with the entertainment items that match the given release date
+   *     ordering, the list will be empty if no items were found
+   */
+  public EntertainmentItemList
+  queryItemsByReleaseDate(FetchOptions fetchOptions,
+                          SortDirection sortDirection) {
+    return createItemListFromQuery(
+        fetchOptions, new Query(ENTERTAINMENT_ITEM_KIND)
+                          .addSort(RELEASE_DATE_TIMESTAMP_MILLIS_PROPERTY_KEY,
+                                   sortDirection));
+  }
+
+  private Optional<EntertainmentItem>
+  queryItemByProperty(String propertyName, Object propertyValue) {
+    Query query = new Query(ENTERTAINMENT_ITEM_KIND)
+                      .setFilter(new FilterPredicate(
+                          propertyName, FilterOperator.EQUAL, propertyValue));
     PreparedQuery queryResults = datastoreService.prepare(query);
 
     Entity itemEntity = queryResults.asSingleEntity();
@@ -160,31 +184,36 @@ public final class EntertainmentItemDatastore {
     return Optional.of(createItemFromEntity(itemEntity));
   }
 
-  private EntertainmentItemList createItemListFromQuery(FetchOptions fetchOptions, Query query) {
+  private EntertainmentItemList
+  createItemListFromQuery(FetchOptions fetchOptions, Query query) {
     PreparedQuery queryResults = datastoreService.prepare(query);
 
-    QueryResultList<Entity> entityList = queryResults.asQueryResultList(fetchOptions);
+    QueryResultList<Entity> entityList =
+        queryResults.asQueryResultList(fetchOptions);
     List<EntertainmentItem> itemList = new ArrayList<>();
 
     for (Entity itemEntity : entityList) {
       itemList.add(createItemFromEntity(itemEntity));
     }
 
-    return new EntertainmentItemList(itemList, entityList.getCursor().toWebSafeString());
+    return new EntertainmentItemList(itemList,
+                                     entityList.getCursor().toWebSafeString());
   }
 
   private static EntertainmentItem createItemFromEntity(Entity itemEntity) {
     Long uniqueId = itemEntity.getKey().getId();
-    String title = (String) itemEntity.getProperty(DISPLAY_TITLE_PROPERTY_KEY);
-    String description = (String) itemEntity.getProperty(DESCRIPTION_PROPERTY_KEY);
-    String imageUrl = (String) itemEntity.getProperty(IMAGE_URL_PROPERTY_KEY);
-    String releaseDate = (String) itemEntity.getProperty(RELEASE_DATE_PROPERTY_KEY);
-    String runtime = (String) itemEntity.getProperty(RUNTIME_PROPERTY_KEY);
-    String genre = (String) itemEntity.getProperty(GENRE_PROPERTY_KEY);
-    String directors = (String) itemEntity.getProperty(DIRECTORS_PROPERTY_KEY);
-    String writers = (String) itemEntity.getProperty(WRITERS_PROPERTY_KEY);
-    String actors = (String) itemEntity.getProperty(ACTORS_PROPERTY_KEY);
-    String omdbId = (String) itemEntity.getProperty(OMDB_ID_PROPERTY_KEY);
+    String title = (String)itemEntity.getProperty(DISPLAY_TITLE_PROPERTY_KEY);
+    String description =
+        (String)itemEntity.getProperty(DESCRIPTION_PROPERTY_KEY);
+    String imageUrl = (String)itemEntity.getProperty(IMAGE_URL_PROPERTY_KEY);
+    String releaseDate =
+        (String)itemEntity.getProperty(RELEASE_DATE_PROPERTY_KEY);
+    String runtime = (String)itemEntity.getProperty(RUNTIME_PROPERTY_KEY);
+    String genre = (String)itemEntity.getProperty(GENRE_PROPERTY_KEY);
+    String directors = (String)itemEntity.getProperty(DIRECTORS_PROPERTY_KEY);
+    String writers = (String)itemEntity.getProperty(WRITERS_PROPERTY_KEY);
+    String actors = (String)itemEntity.getProperty(ACTORS_PROPERTY_KEY);
+    String omdbId = (String)itemEntity.getProperty(OMDB_ID_PROPERTY_KEY);
 
     return new EntertainmentItem.Builder()
         .setUniqueId(uniqueId)
