c6fd45db8fe6ca6a65be13c6038fce4eefaf9082
Oyin Sholeye <oyin.s@wustl.edu>
2020-07-14 19:56:29 +0000

    Simplified to one function in user_script.js

--- src/main/webapp/user_script.js
+++ src/main/webapp/user_script.js
@@ -1,45 +1,22 @@
 /**
  * Fetches the json from ProfileServlet and displays the values on the Profile
- * Page.
+ * Page. Also prepopulates the values on the Edit Profile Page.
  */
 function loadProfile() {
   fetch('/profile-data')
-    .then((response) => response.json())
-    .then((profile) => {
-      const nameSection = document.getElementById('name');
-      nameSection.innerHTML = profile.name;
-      nameSection.value=profile.name;
-
-      const usernameSection = document.getElementById('username');
-      usernameSection.innerHTML = profile.username;
-      usernameSection.innerHTML = profile.username;
-
-      const bioSection = document.getElementById('bio');
-      bioSection.innerHTML = profile.bio;
-    })
-    .catch((error) => {
-      console.log('Fetching profile data servlet failed: ' + error);
-    });
-}
-
-/**
- * Fetches the json from ProfileServlet and prepoopulates the values on the Edit Profile
- * Page.
- */
-function loadPopulatedProfile() {
-  fetch('/profile-data')
-    .then((response) => response.json())
-    .then((profile) => {
-      const nameSection = document.getElementById('name');
-      nameSection.value=profile.name;
-
-      const usernameSection = document.getElementById('username');
-      usernameSection.value = profile.username;
-      
-      const bioSection = document.getElementById('bio');
-      bioSection.value = profile.bio;
-    })
-    .catch((error) => {
-      console.log('Fetching profile data servlet failed: ' + error);
-    });
+      .then((response) => response.json())
+      .then((profile) => {
+        const nameSection = document.getElementById('name');
+        nameSection.innerHTML = profile.name;
+        nameSection.value = profile.name;
+        const usernameSection = document.getElementById('username');
+        usernameSection.innerHTML = profile.username;
+        usernameSection.value = profile.username;
+        const bioSection = document.getElementById('bio');
+        bioSection.innerHTML = profile.bio;
+        bioSection.value = profile.bio;
+      })
+      .catch((error) => {
+        console.log('Fetching profile data servlet failed: ' + error);
+      });
 }
