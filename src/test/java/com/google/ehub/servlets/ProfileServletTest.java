fb6a96efe58a4d70e2f21e6957c44cb87aed92b7
Oyin Sholeye <oyin.s@wustl.edu>
2020-07-15 17:08:30 +0000

    Updated LoginServlet, ProfileServlet and ProfileServletTest

--- src/test/java/com/google/ehub/servlets/ProfileServletTest.java
+++ src/test/java/com/google/ehub/servlets/ProfileServletTest.java
@@ -4,7 +4,6 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
