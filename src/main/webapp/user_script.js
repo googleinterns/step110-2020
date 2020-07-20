ee799a169702c45dc715bc3d99a4a2ebb2e3adc1
Oyin Sholeye <oyin.s@wustl.edu>
2020-07-20 20:00:31 +0000

    Modified user script

--- src/main/webapp/user_script.js
+++ src/main/webapp/user_script.js
@@ -3,20 +3,20 @@
  * Page. Also prepopulates the values on the Edit Profile Page.
  */
 function loadProfile() {
-  fetch('/profile-data')
+  fetch("/profile-data")
     .then((response) => response.json())
     .then((profile) => {
       if (profile.NeedsProfile) {
         window.location.replace("/CreateProfilePage.html");
         return;
       }
-      const nameSection = document.getElementById('name');
+      const nameSection = document.getElementById("name");
       nameSection.innerHTML = profile.name;
       nameSection.value = profile.name;
-      const usernameSection = document.getElementById('username');
+      const usernameSection = document.getElementById("username");
       usernameSection.innerHTML = profile.username;
       usernameSection.value = profile.username;
-      const bioSection = document.getElementById('bio');
+      const bioSection = document.getElementById("bio");
       bioSection.innerHTML = profile.bio;
       bioSection.value = profile.bio;
       const profileImage = document.getElementById("avatar");
@@ -25,28 +25,26 @@ function loadProfile() {
       profileImage.src = "https://icotar.com/avatar/" + avatarLetter;
     })
     .catch((error) => {
-      console.log('Fetching profile data servlet failed: ' + error);
+      console.log("Fetching profile data servlet failed: " + error);
     });
-  $(document).ready(function() {
-    $('#navbar').load('navbar.html', function() {
-      $('#navbarProfileSection').addClass('d-none');
+  $(document).ready(function () {
+    $("#navbar").load("navbar.html", function () {
+      $("#navbarProfileSection").addClass("d-none");
     });
   });
 }
 
-/**Fetches the items that were liked by user and sends the array to 
-* populate the page with the item cards.
-*/
+/**Fetches the items that were liked by user and sends the array to
+ * populate the page with the item cards.
+ */
 function loadFavItems() {
-  fetch('/favorite-item')
+  fetch("/favorite-item")
     .then((response) => response.json())
     .then((favorites) => {
-      var itemIdArray = new Array();
-      itemIdArray = favorites;
-      populateFavoriteItemGrid(itemIdArray)
+      populateFavoriteItemGrid(favorites);
     })
     .catch((error) => {
-      console.log('Fetching favorite item data servlet failed: ' + error);
+      console.log("Fetching favorite item data servlet failed: " + error);
     });
 }
 
@@ -56,16 +54,17 @@ function loadFavItems() {
  * @param { Array } favoriteItems - the array of item ids
  */
 function populateFavoriteItemGrid(favoriteItems) {
-  const favItemContainer = $('#item-container');
+  const favItemContainer = $("#item-container");
+  const rowElem = $('<div class="row"></div>');
+  const MAX_CELLS_PER_ROW = 3;
   let currItemIndex = 0;
 
   while (currItemIndex < favoriteItems.length) {
- 
-    const rowElem = $('<div class="row"></div>');
-
-    const MAX_CELLS_PER_ROW = 3;
-
-    for (let cell = 0;cell < MAX_CELLS_PER_ROW && currItemIndex < favoriteItems.length; cell++, currItemIndex++) {
+    for (
+      let cell = 0;
+      cell < MAX_CELLS_PER_ROW && currItemIndex < favoriteItems.length;
+      cell++, currItemIndex++
+    ) {
       const item = favoriteItems[currItemIndex];
       const colElem = $('<div class="col-6 col-md-4"</div>');
 
@@ -74,12 +73,11 @@ function populateFavoriteItemGrid(favoriteItems) {
         .then((itemPageData) => {
           colElem.append(createFavoriteItemCard(itemPageData.item));
           rowElem.append(colElem);
-        })
+        });
     }
 
     favItemContainer.append(rowElem);
   }
-
 }
 
 /**
@@ -91,12 +89,19 @@ function populateFavoriteItemGrid(favoriteItems) {
 function createFavoriteItemCard(entertainmentItem) {
   const card = $('<div class="mt-2" class="card bg-light"></div>');
   card.append(
-    $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">'));
+    $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">')
+  );
   const cardBody = $('<div class="card-body"></div>');
   cardBody.append(
-    $('<h5 class="card-title">' + entertainmentItem.title + '(' +
-      entertainmentItem.releaseDate + ')' +
-      '</h5>'));
+    $(
+      '<h5 class="card-title">' +
+        entertainmentItem.title +
+        "(" +
+        entertainmentItem.releaseDate +
+        ")" +
+        "</h5>"
+    )
+  );
   card.append(cardBody);
 
   return card;
