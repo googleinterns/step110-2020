bb7622328d88ee094835828fc509e8bd139ea412
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-07-20 17:58:30 +0000

    Add Asserts to check that the number of likes in an entertainment item was added or removed

--- src/test/java/com/google/ehub/servlets/FavoriteItemServletTest.java
+++ src/test/java/com/google/ehub/servlets/FavoriteItemServletTest.java
@@ -10,10 +10,13 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.ehub.data.EntertainmentItem;
+import com.google.ehub.data.EntertainmentItemDatastore;
 import com.google.ehub.data.FavoriteItemDatastore;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
@@ -29,16 +32,20 @@ import org.mockito.MockitoAnnotations;
 public class FavoriteItemServletTest {
   private static final String FAVORITE_ITEM_ID_PARAMETER_KEY = "favoriteItemId";
   private static final String ENTERTAINMENT_ITEM_KIND = "entertainmentItem";
+  private static final String NUMBER_OF_LIKES_PROPERTY_KEY = "numberOfLikes";
   private static final String JSON_CONTENT_TYPE = "application/json";
 
   private static final String EMAIL = "Bryan@gmail.com";
   private static final String VALID_ITEM_ID_PARAMETER = "123";
   private static final String INVALID_ITEM_ID_PARAMETER = "wjnrwwoiofwij";
+  private static final Long NUMBER_OF_LIKES = 5L;
 
   private static final Long[] ITEM_IDS = {23L, 44L, 77L, 89L, 94L, 21301L};
 
   private final FavoriteItemServlet servlet = new FavoriteItemServlet();
   private final FavoriteItemDatastore favoriteItemDatastore = FavoriteItemDatastore.getInstance();
+  private final EntertainmentItemDatastore entertainmentItemDatastore =
+      EntertainmentItemDatastore.getInstance();
   private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
   private final LocalServiceTestHelper helper =
       new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig())
@@ -136,6 +143,8 @@ public class FavoriteItemServletTest {
   @Test
   public void postRequestWithValidParamAndItemExists_favoriteItemIsAdded() throws IOException {
     Entity itemEntity = new Entity(ENTERTAINMENT_ITEM_KIND);
+    itemEntity.setProperty(NUMBER_OF_LIKES_PROPERTY_KEY, NUMBER_OF_LIKES);
+
     datastoreService.put(itemEntity);
 
     when(request.getParameter(FAVORITE_ITEM_ID_PARAMETER_KEY))
@@ -143,13 +152,22 @@ public class FavoriteItemServletTest {
 
     servlet.doPost(request, response);
 
-    Assert.assertEquals(1, FavoriteItemDatastore.getInstance().queryFavoriteIds(EMAIL).size());
+    Assert.assertEquals(1, favoriteItemDatastore.queryFavoriteIds(EMAIL).size());
+
+    Optional<EntertainmentItem> entertainmentItem =
+        entertainmentItemDatastore.queryItem(itemEntity.getKey().getId());
+
+    Assert.assertTrue(entertainmentItem.isPresent());
+    Assert.assertEquals(
+        Long.valueOf(NUMBER_OF_LIKES + 1L), entertainmentItem.get().getNumberOfLikes());
   }
 
   @Test
   public void postRequestWithValidParamButUserNotLoggedIn_favoriteItemIsNotAdded()
       throws IOException {
     Entity itemEntity = new Entity(ENTERTAINMENT_ITEM_KIND);
+    itemEntity.setProperty(NUMBER_OF_LIKES_PROPERTY_KEY, NUMBER_OF_LIKES);
+
     datastoreService.put(itemEntity);
 
     helper.setEnvIsLoggedIn(false);
@@ -160,6 +178,12 @@ public class FavoriteItemServletTest {
     servlet.doPost(request, response);
 
     Assert.assertTrue(favoriteItemDatastore.queryEmails(itemEntity.getKey().getId()).isEmpty());
+
+    Optional<EntertainmentItem> entertainmentItem =
+        entertainmentItemDatastore.queryItem(itemEntity.getKey().getId());
+
+    Assert.assertTrue(entertainmentItem.isPresent());
+    Assert.assertEquals(NUMBER_OF_LIKES, entertainmentItem.get().getNumberOfLikes());
   }
 
   @Test
@@ -204,15 +228,26 @@ public class FavoriteItemServletTest {
   @Test
   public void deleteRequestWithValidParamAndUserIsLoggedIn_favoriteItemEntityIsDeleted()
       throws IOException {
-    favoriteItemDatastore.addFavoriteItem(EMAIL, ITEM_IDS[0]);
+    Entity itemEntity = new Entity(ENTERTAINMENT_ITEM_KIND);
+    itemEntity.setProperty(NUMBER_OF_LIKES_PROPERTY_KEY, NUMBER_OF_LIKES);
 
-    when(request.getParameter(FAVORITE_ITEM_ID_PARAMETER_KEY))
-        .thenReturn(String.valueOf(ITEM_IDS[0]));
+    datastoreService.put(itemEntity);
 
-    Assert.assertEquals(1, favoriteItemDatastore.queryEmails(ITEM_IDS[0]).size());
+    final Long itemId = itemEntity.getKey().getId();
+
+    favoriteItemDatastore.addFavoriteItem(EMAIL, itemId);
+
+    when(request.getParameter(FAVORITE_ITEM_ID_PARAMETER_KEY)).thenReturn(String.valueOf(itemId));
+
+    Assert.assertEquals(1, favoriteItemDatastore.queryEmails(itemId).size());
 
     servlet.doDelete(request, response);
 
-    Assert.assertTrue(favoriteItemDatastore.queryEmails(ITEM_IDS[0]).isEmpty());
+    Assert.assertTrue(favoriteItemDatastore.queryEmails(itemId).isEmpty());
+
+    Optional<EntertainmentItem> entertainmentItem = entertainmentItemDatastore.queryItem(itemId);
+
+    Assert.assertTrue(entertainmentItem.isPresent());
+    Assert.assertEquals(NUMBER_OF_LIKES, entertainmentItem.get().getNumberOfLikes());
   }
 }
