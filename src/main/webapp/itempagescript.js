c0c5b0ee69559147b59b9e880d106a027b1140f3
Oyin Sholeye <oyin.s@wustl.edu>
2020-07-21 16:15:42 +0000

    I ran prettier on all files. Files were not modified

--- src/main/webapp/itempagescript.js
+++ src/main/webapp/itempagescript.js
@@ -2,21 +2,21 @@
  * Retrieves ItemPageData and forms page using other functions.
  */
 async function loadItemPage() {
-  $(document).ready(function() {
-    $('#navbar').load('navbar.html', function() {
+  $(document).ready(function () {
+    $("#navbar").load("navbar.html", function () {
       initializeNavBarProfileSection();
     });
-    const itemId = getUrlParam('itemId');
+    const itemId = getUrlParam("itemId");
 
-    if (itemId !== '') {
+    if (itemId !== "") {
       fetch(`/itempagedata?itemId=${itemId}`)
-          .then((response) => response.json())
-          .then((itemPageData) => {
-            createSelectedItemCard(itemPageData.item);
-            getItemPageComments(itemPageData.comments);
-          });
+        .then((response) => response.json())
+        .then((itemPageData) => {
+          createSelectedItemCard(itemPageData.item);
+          getItemPageComments(itemPageData.comments);
+        });
     } else {
-      console.log('ItemId is empty!');
+      console.log("ItemId is empty!");
     }
   });
 }
@@ -27,20 +27,32 @@ async function loadItemPage() {
 async function createSelectedItemCard(entertainmentItem) {
   const card = $('<div class="mt-2" class="card bg-light"></div>');
   card.append(
-      $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">'));
+    $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">')
+  );
   const cardBody = $('<div class="card-body"></div>');
   cardBody.append(
-      $('<h5 class="card-title">' + entertainmentItem.title + '(' +
-        entertainmentItem.releaseDate + ')' +
-        '</h5>'));
+    $(
+      '<h5 class="card-title">' +
+        entertainmentItem.title +
+        "(" +
+        entertainmentItem.releaseDate +
+        ")" +
+        "</h5>"
+    )
+  );
   cardBody.append(
-      $('<h5 class="card-title">' + entertainmentItem.genre + '</h5>'));
+    $('<h5 class="card-title">' + entertainmentItem.genre + "</h5>")
+  );
   cardBody.append(
-      $('<p class="card-text"><b>Description: </b>' +
-        entertainmentItem.description + '</p>'));
+    $(
+      '<p class="card-text"><b>Description: </b>' +
+        entertainmentItem.description +
+        "</p>"
+    )
+  );
 
   card.append(cardBody);
-  const itemContainer = $('#item-container');
+  const itemContainer = $("#item-container");
   itemContainer.append(card);
 }
 
@@ -48,16 +60,16 @@ async function createSelectedItemCard(entertainmentItem) {
  * Sends comment data and ItemId to Servlet
  */
 async function sendFormData() {
-  const comment = $('#comment').val();
-  const itemId = getUrlParam('itemId');
-  if (comment != '' && itemId != null) {
-    $.post('/itempagedata', {comment: comment, itemId: itemId})
-        .done(function() {
-          window.location.reload();
-        })
-        .fail(function() {
-          console.log('Failed to send form data');
-        });
+  const comment = $("#comment").val();
+  const itemId = getUrlParam("itemId");
+  if (comment != "" && itemId != null) {
+    $.post("/itempagedata", { comment: comment, itemId: itemId })
+      .done(function () {
+        window.location.reload();
+      })
+      .fail(function () {
+        console.log("Failed to send form data");
+      });
   }
 }
 
@@ -65,12 +77,14 @@ async function sendFormData() {
  * Function which builds the comment element from the ItemPageData object
  */
 function getItemPageComments(comments) {
-  const commentContainer = $('#comment-container');
+  const commentContainer = $("#comment-container");
   comments.forEach((commentDataManager) => {
     const date = new Date(commentDataManager.timestampMillis);
-    commentContainer.append(createListElement(
-        commentDataManager.comment + ' - ' +
-        '(' + date.toLocaleString() + ')'));
+    commentContainer.append(
+      createListElement(
+        commentDataManager.comment + " - " + "(" + date.toLocaleString() + ")"
+      )
+    );
   });
 }
 
@@ -78,8 +92,8 @@ function getItemPageComments(comments) {
  * Creates list element which houses comments.
  */
 function createListElement(comment) {
-  const liElement = document.createElement('li');
-  liElement.className = 'list-group-item';
+  const liElement = document.createElement("li");
+  liElement.className = "list-group-item";
   liElement.append(comment);
   return liElement;
 }
@@ -97,5 +111,5 @@ function getUrlParam(param) {
     return urlParams.get(param);
   }
 
-  return '';
+  return "";
 }
