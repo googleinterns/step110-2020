1246e4227f1bd197629fe6c05117aa6a9a2c844a
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-07-21 21:28:17 +0000

    Modify JSON object response to hold user profile data as well as recommendation list

--- src/main/webapp/user_script.js
+++ src/main/webapp/user_script.js
@@ -3,33 +3,36 @@
  * Page. Also prepopulates the values on the Edit Profile Page.
  */
 function loadProfile() {
-  fetch("/profile-data")
-    .then((response) => response.json())
-    .then((profile) => {
-      if (profile.NeedsProfile) {
-        window.location.replace("/CreateProfilePage.html");
-        return;
-      }
-      const nameSection = document.getElementById("name");
-      nameSection.innerHTML = profile.name;
-      nameSection.value = profile.name;
-      const usernameSection = document.getElementById("username");
-      usernameSection.innerHTML = profile.username;
-      usernameSection.value = profile.username;
-      const bioSection = document.getElementById("bio");
-      bioSection.innerHTML = profile.bio;
-      bioSection.value = profile.bio;
-      const profileImage = document.getElementById("avatar");
-      const username = profile.username;
-      const avatarLetter = username.charAt(0);
-      profileImage.src = "https://icotar.com/avatar/" + avatarLetter;
-    })
-    .catch((error) => {
-      console.log("Fetching profile data servlet failed: " + error);
-    });
-  $(document).ready(function () {
-    $("#navbar").load("navbar.html", function () {
-      $("#navbarProfileSection").addClass("d-none");
+  fetch('/profile-data')
+      .then((response) => response.json())
+      .then((userData) => {
+        if(userData.NeedsProfile){
+          window.location.replace("/CreateProfilePage.html");
+          return;
+        }
+
+        const profile = userData.profile;
+
+        const nameSection = document.getElementById('name');
+        nameSection.innerHTML = profile.name;
+        nameSection.value = profile.name;
+        const usernameSection = document.getElementById('username');
+        usernameSection.innerHTML = profile.username;
+        usernameSection.value = profile.username;
+        const bioSection = document.getElementById('bio');
+        bioSection.innerHTML = profile.bio;
+        bioSection.value = profile.bio;
+        const profileImage = document.getElementById("avatar");
+        const username = profile.username;
+        const avatarLetter = username.charAt(0);
+        profileImage.src = "https://icotar.com/avatar/" + avatarLetter;
+      })
+      .catch((error) => {
+        console.log('Fetching profile data servlet failed: ' + error);
+      });
+  $(document).ready(function() {
+    $('#navbar').load('navbar.html', function() {
+      $('#navbarProfileSection').addClass('d-none');
     });
   });
 }
