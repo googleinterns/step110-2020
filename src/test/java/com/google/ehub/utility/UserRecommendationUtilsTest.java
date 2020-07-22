bb72c350b84328a03a3ff88a566a2f9658633627
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-07-22 15:54:58 +0000

    Add blueprints for UserRecommendationUtilsTest

---
+++ src/test/java/com/google/ehub/utility/UserRecommendationUtilsTest.java
@@ -0,0 +1,26 @@
+package com.google.ehub.utility;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UserRecommendationUtilsTest {
+  private static final int MAX_RECOMMENDED_USERS = 10;
+  
+  @Test
+  public void getRecommendedEmailsFromEmptyList_returnsEmptyList() {
+
+  }
+
+  @Test
+  public void getRecommendedEmailsFromSingleUserList_returnsListWithSingleUser() {
+
+  }
+
+  @Test
+  public void getRecommendedEmailsFromListWithMoreThanMaxRecommendedUsers_returnsListWithMaxRecommendedUsers() {
+
+  }
+}
