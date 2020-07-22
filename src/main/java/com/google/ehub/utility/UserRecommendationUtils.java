9e2fd8f19a7710ea3353866d931352f97fce4980
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-07-22 15:54:29 +0000

    Separate algorithm steps into multiple methods

--- src/main/java/com/google/ehub/utility/UserRecommendationUtils.java
+++ src/main/java/com/google/ehub/utility/UserRecommendationUtils.java
@@ -1,10 +1,12 @@
 package com.google.ehub.utility;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.PriorityQueue;
 
 /**
  * Utility class that implements the algorithm used to recommend the users that have the most common
@@ -23,10 +25,47 @@ public final class UserRecommendationUtils {
   public List<String> getRecommendedEmails(Map<Long, Set<String>> itemLikes) {
     List<String> recommendedEmails = new ArrayList<String>();
 
-    // TODO: Implement algorithm that finds the users that have the highest amount of common items,
-    // could be implemented
-    // by using a Map<String, int> storing counts and a priority queue to get the
-    // "maxNumberOfRecommendations"
+    return recommendedEmails;
+  }
+
+  private Map<String, Integer> getEmailFreqs(Map<Long, List<String>> itemLikes, String userEmail) {
+    Map<String, Integer> emailFreqs = new HashMap<String, Integer>();
+
+    for (Map.Entry<Long, List<String>> emailGroup : itemLikes.entrySet()) {
+      for (String email : emailGroup.getValue()) {
+        emailFreqs.merge(email, 1, (oldFreq, deltaFreq) -> oldFreq + deltaFreq);
+      }
+    }
+
+    return emailFreqs;
+  }
+
+  private PriorityQueue<Map.Entry<String, Integer>> getEmailMinHeap(
+      Map<String, Integer> emailFreqs) {
+    PriorityQueue<Map.Entry<String, Integer>> emailMinHeap =
+        new PriorityQueue<>((a, b) -> Integer.compare(a.getValue(), b.getValue()));
+
+    for (Map.Entry<String, Integer> entry : emailFreqs.entrySet()) {
+      emailMinHeap.offer(entry);
+
+      if (emailMinHeap.size() > MAX_NUMBER_OF_RECOMMENDATIONS) {
+        emailMinHeap.poll();
+      }
+    }
+
+    return emailMinHeap;
+  }
+
+  private List<String> getRecommendedEmailsInDescendingOrder(
+      PriorityQueue<Map.Entry<String, Integer>> emailMinHeap) {
+    List<String> recommendedEmails = new ArrayList<String>();
+
+    while (!emailMinHeap.isEmpty()) {
+      recommendedEmails.add(emailMinHeap.poll().getKey());
+    }
+
+    // The most recommended emails should come first.
+    Collections.reverse(recommendedEmails);
 
     return recommendedEmails;
   }
