344a517a417380dd7d2efb8d245b6e696657e4d8
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-07-27 16:57:02 +0000

    Move form validation from HTML files into JS files

---
+++ src/main/webapp/edit-profile-page.js
@@ -0,0 +1,36 @@
+function loadEditProfileValues() {
+  fetch('/profile-data')
+      .then((response) => response.json())
+      .then((userData) => {
+        const profile = userData.profile;
+
+        $('#name').val(profile.name);
+        $('#username').val(profile.username);
+        $('#bio').val(profile.bio);
+      })
+      .catch((error) => {
+        console.log('Fetching profile data servlet failed: ' + error);
+      });
+}
+
+function loadNavBar() {
+  $(document).ready(function() {
+    $('#navbar').load('navbar.html', function() {
+      initializeNavBarProfileSection();
+    });
+  });
+}
+
+function addProfileFormValidation() {
+  const forms = document.getElementsByClassName('needs-validation');
+
+  const validation = Array.prototype.filter.call(forms, (form) => {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  }, false);
+}
