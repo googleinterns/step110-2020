d20f9c187420faee162698515cc7efbeb73527f5
Oyin Sholeye <oyin.s@wustl.edu>
2020-07-29 16:37:34 +0000

    Formatted and added param

--- src/main/webapp/itempagescript.js
+++ src/main/webapp/itempagescript.js
@@ -2,13 +2,13 @@
  * Retrieves ItemPageData and forms page using other functions.
  */
 async function loadItemPage() {
-  $(document).ready(function() {
-    $('#navbar').load('navbar.html', function() {
+  $(document).ready(function () {
+    $("#navbar").load("navbar.html", function () {
       initializeNavBarProfileSection();
     });
-    const itemId = getUrlParam('itemId');
+    const itemId = getUrlParam("itemId");
 
-    if (itemId !== '') {
+    if (itemId !== "") {
       fetch(`/itempagedata?itemId=${itemId}`)
         .then((response) => response.json())
         .then((itemPageData) => {
@@ -23,8 +23,8 @@ async function loadItemPage() {
                 login.append(
                   $(
                     '<a href="' +
-                    loginResponse.LoginURL +
-                    '"><button type="button" class="btn btn-dark my-sm-0">Login to Comment</button></a>'
+                      loginResponse.LoginURL +
+                      '"><button type="button" class="btn btn-dark my-sm-0">Login to Comment</button></a>'
                   )
                 );
               }
@@ -34,7 +34,7 @@ async function loadItemPage() {
             });
         });
     } else {
-      console.log('ItemId is empty!');
+      console.log("ItemId is empty!");
     }
   });
 }
@@ -57,45 +57,47 @@ function hideCommentBox() {
 async function createSelectedItemCard(entertainmentItem) {
   const card = $('<div class="mt-2" class="card bg-light"></div>');
   card.append(
-      $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">'));
+    $('<img class="card-img-top" src="' + entertainmentItem.imageUrl + '">')
+  );
   const cardBody = $('<div class="card-body"></div>');
   cardBody.append(
     $(
       '<h5 class="card-title">' +
-      entertainmentItem.title +
-      "(" +
-      entertainmentItem.releaseDate +
-      ")" +
-      "</h5>"
+        entertainmentItem.title +
+        "(" +
+        entertainmentItem.releaseDate +
+        ")" +
+        "</h5>"
     )
   );
 
   cardBody.append(
     $(
       '<h5 class="card-title"><b>Runtime: </b>' +
-      entertainmentItem.runtime +
-      "</h5>"
+        entertainmentItem.runtime +
+        "</h5>"
     )
   );
   cardBody.append(
-      $('<h5 class="card-title">' + entertainmentItem.genre + '</h5>'));
+    $('<h5 class="card-title">' + entertainmentItem.genre + "</h5>")
+  );
   cardBody.append(
     $(
       '<h5 class="card-title"><b>Cast: </b>' +
-      entertainmentItem.actors +
-      "</h5>"
+        entertainmentItem.actors +
+        "</h5>"
     )
   );
 
   cardBody.append(
     $(
       '<p class="card-text"><b>Description: </b>' +
-      entertainmentItem.description +
-      "</p>"
+        entertainmentItem.description +
+        "</p>"
     )
   );
   card.append(cardBody);
-  const itemContainer = $('#item-container');
+  const itemContainer = $("#item-container");
   itemContainer.append(card);
 }
 
@@ -107,10 +109,10 @@ async function sendFormData() {
   const itemId = getUrlParam("itemId");
   if (comment != "" && itemId != null) {
     $.post("/itempagedata", { comment: comment, itemId: itemId })
-      .done(function() {
+      .done(function () {
         window.location.reload();
       })
-      .fail(function() {
+      .fail(function () {
         console.log("Failed to send form data");
       });
   }
@@ -120,20 +122,22 @@ async function sendFormData() {
  * Function which builds the comment element from the ItemPageData object
  */
 function getItemPageComments(comments) {
-  const commentContainer = $('#comment-container');
+  const commentContainer = $("#comment-container");
   comments.forEach((commentDataManager) => {
     const date = new Date(commentDataManager.timestampMillis);
     const commentId = commentDataManager.commentId;
     commentContainer.append(
       createListElement(
         commentDataManager.username +
-        ": " +
-        commentDataManager.comment +
-        " - " +
-        "(" +
-        date.toLocaleString() +
-        ")", commentDataManager.belongsToUser
-      , commentDataManager.email)
+          ": " +
+          commentDataManager.comment +
+          " - " +
+          "(" +
+          date.toLocaleString() +
+          ")",
+        commentDataManager.belongsToUser,
+        commentDataManager.email
+      )
     );
   });
 }
@@ -143,15 +147,24 @@ function getItemPageComments(comments) {
  *
  * @param { string } comment - the comment including the username, message, and timestamp
  * @param { boolean } belongsToUser - boolean that shows whether or not the current user has posted a comment
+ * @param { string } email - the email of the user
  * @returns { html element }  returns a list element
  */
 function createListElement(comment, belongsToUser, email) {
   const liElement = $('<li class="list-group-item"></li>');
   liElement.text(comment);
   if (belongsToUser) {
-    liElement.append($('<i class="fa fa-trash" style="float:right;"></i>')); 
+    liElement.append($('<i class="fa fa-trash" style="float:right;"></i>'));
   }
-    liElement.append($('<img class="pr-1" src="'+ "https://icotar.com/avatar/"+ email.charAt(0) +".png?s=23" + '"style=float:left;"></img>'));
+  liElement.append(
+    $(
+      '<img class="pr-1" src="' +
+        "https://icotar.com/avatar/" +
+        email.charAt(0) +
+        ".png?s=23" +
+        '"style=float:left;"></img>'
+    )
+  );
   return liElement;
 }
 
@@ -168,5 +181,5 @@ function getUrlParam(param) {
     return urlParams.get(param);
   }
 
-  return '';
+  return "";
 }
